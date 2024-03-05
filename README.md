8cf2a5f650357f427a5a2b3cfe67090c4313ff2c
Any97Cris <criscianysilva1997@gmail.com>
2024-03-05 14:43:31 -0400

    primeiros passos php

---
+++ README.md
@@ -0,0 +1,2 @@
+## Exemplos com base no livro Learn PHP in 24 hours or less - Robert Dwight
+
---
+++ php_no_html.php
@@ -0,0 +1,17 @@
+<?php
+    $nome = "Fulano";
+    $cor_favorita = "cinza";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usar PHP no HTML</title>
+</head>
+<body>
+    <h1>Nome: <?= $nome ?></h1>
+    <h1>Cor favorita: <?= $nome ?></h1>
+</body>
+</html>
