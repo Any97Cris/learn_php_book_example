b287fc6da401ca7d92415bb36c8b84b30ff4a292
Any97Cris <criscianysilva1997@gmail.com>
2024-03-12 14:13:56 -0400

    estrutura loop, while, do while, for

---
+++ estrutura_loop.php
@@ -0,0 +1,20 @@
+<?php
+
+#loop While
+// $count = 1;
+// while($count < 10){
+//     print $count++ ."<br>";
+// };
+
+#Loop Do While
+// do{
+//     if($count){
+//         print $count++;
+//     }
+
+// }while(false);
+
+#Loop For
+// for($cont = 0;$cont < 10; $cont++){
+//     print $cont;
+// }
